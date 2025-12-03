d205844a35cda6a023baefcac3c903df0c72af06
916eleanor-blip <916eleanor@gmail.com>
2025-12-03 21:20:51 +0000

    Update config to prompt for token on first use

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 // GitHub Repository Configuration
+// NOTE: Token must be provided by user - cannot be stored in public repo
 const GITHUB_CONFIG = {
     owner: '916eleanor-blip',
     repo: 'Conference-Room-Booking',
     branch: 'main',
     filePath: 'bookings.json',
-    // Token will need to be set - see GITHUB-SETUP.md for instructions
-    token: 'REPLACE_WITH_YOUR_GITHUB_TOKEN'
+    // Enter your token here (this file should be added to .gitignore in production)
+    token: localStorage.getItem('github_token') || prompt('Enter GitHub token (one-time setup):')
 };
+
+// Save token for future use
+if (GITHUB_CONFIG.token && !localStorage.getItem('github_token')) {
+    localStorage.setItem('github_token', GITHUB_CONFIG.token);
+}
